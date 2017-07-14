85105e932099e18abbff18fd3ef139151942a2c9
konstantingrusin <kgrushin@ncem.ru>
2017-07-14 13:52:24 +0300

    first step

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
 <template>
     <v-app light>
         <v-toolbar>
-            <v-toolbar-title></v-toolbar-title>
+            <v-toolbar-title>Meetup</v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-toolbar-items>
+                <v-btn raised>
+                    <v-icon fa>search</v-icon>
+                    View Meetups
+                </v-btn>
+            </v-toolbar-items>
         </v-toolbar>
         <main>
 
